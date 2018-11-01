5dd0180b01a3c4ef3e86ac86c28208ce2dc5b5f0
Sander de Vries <sanderdv@icloud.com>
2018-11-01 18:24:49 +0100

    Light - project App Studio

--- Light/ViewController.swift
+++ Light/ViewController.swift
@@ -7,14 +7,63 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
-
+    
+    // Variables
+    var lightOn = true
+    
+    // Actions
+    @IBAction func buttonPressed(_ sender: Any) {
+        
+        // change lightOn status and change background
+        lightOn = !lightOn
+        updateUI()
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        updateUI()
     }
-
-
+    
+    func updateUI() {
+        view.backgroundColor = lightOn ? .white : .black
+        toggleFlash()
+        
+    }
+    
+    func toggleFlash() {
+        
+        // initiate media device to adjust flash
+        guard let flashDevice = AVCaptureDevice.default(for: .video) else {
+            print("AVCaptureDevice object cannot be initialized")
+            return
+        }
+        
+        if flashDevice.hasTorch && flashDevice.isTorchAvailable {
+            do {
+                
+                // get lock before setting properties
+                try flashDevice.lockForConfiguration()
+                
+                if lightOn {
+                    flashDevice.torchMode = .on
+                } else {
+                    flashDevice.torchMode = .off
+                }
+                
+                // release lock
+                flashDevice.unlockForConfiguration()
+                
+                // lock failed
+            } catch {
+                print("Flash cannot be used")
+            }
+            
+            // device's torch is not available
+        } else {
+            print("Flash is not available")
+        }
+    }
 }
-
